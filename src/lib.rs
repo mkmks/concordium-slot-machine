da48dbe3ee750fda9fdc8680e86bf450a9f52cc7
Doris <DOBEN@users.noreply.github.com>
2022-09-20 11:51:23 +0300

    first function

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
-//! Piggy bank smart contract.
-//!
-//! Allows anyone to insert CCD, but only the owner can "smash" it and
-//! retrieve the CCD. Prevents more CCD to be inserted after being smashed.
+//! Slot machine smart contract.
 //!
 //! This smart contract module is developed as part of the
-//! [Piggy Bank Tutorial](https://developer.concordium.software/en/mainnet/smart-contracts/tutorials/piggy-bank).
 //!
 //! Covers:
 //! - Reading owner, sender, and self_balance from the context and host.
@@ -16,69 +12,62 @@
 // Pulling in everything from the smart contract standard library.
 use concordium_std::*;
 
-/// The state of the piggy bank
+/// The state of the slot machine
 #[derive(Debug, Serialize, PartialEq, Eq, Clone, Copy)]
-enum PiggyBankState {
-    /// Alive and well, allows for CCD to be inserted.
+enum SlotMachineEnum {
+    /// People can insert some CCD to play a game.
     Intact,
-    /// The piggy bank has been emptied, preventing further CCD to be inserted.
-    Smashed,
+    /// Someone played a game. Now waiting for oracle.
+    ActiveGame,
+    /// The slot machine paid out rewards.
+    PaidOut,
 }
 
-/// Setup a new Intact piggy bank.
-#[init(contract = "PiggyBank")]
-fn piggy_init<S: HasStateApi>(
+#[derive(Debug, Serialize, PartialEq, Eq, Clone, Copy)]
+struct SlotMachineState {
+    state: SlotMachineEnum,
+    user_random_value: u8,
+    oracle_random_value: u8,
+}
+
+/// Setup a new Intact slot machine.
+#[init(contract = "SlotMachine")]
+fn slot_machine_init<S: HasStateApi>(
     _ctx: &impl HasInitContext,
     _state_builder: &mut StateBuilder<S>,
-) -> InitResult<PiggyBankState> {
+) -> InitResult<SlotMachineState> {
     // Always succeeds
-    Ok(PiggyBankState::Intact)
+    Ok(SlotMachineState {
+        state: SlotMachineEnum::Intact,
+        user_random_value: 0,
+        oracle_random_value: 0,
+    })
 }
 
-/// Insert some CCD into a piggy bank, allowed by anyone.
-#[receive(contract = "PiggyBank", name = "insert", payable)]
-fn piggy_insert<S: HasStateApi>(
+/// Insert some CCD into a slot machine, allowed by anyone.
+#[receive(contract = "SlotMachine", name = "insert", mutable, payable)]
+fn slot_machine_insert<S: HasStateApi>(
     _ctx: &impl HasReceiveContext,
-    host: &impl HasHost<PiggyBankState, StateApiType = S>,
-    _amount: Amount,
+    host: &mut impl HasHost<SlotMachineState, StateApiType = S>,
+    amount: Amount,
 ) -> ReceiveResult<()> {
-    // Ensure the piggy bank has not been smashed already.
-    ensure!(*host.state() == PiggyBankState::Intact);
-    // Just accept since the CCD balance is managed by the chain.
+    // People have to pay 1 CCD to pull the lever of the slot machine
+    ensure!(amount == Amount { micro_ccd: 1000000 });
+
+    // Now a game is active and we wait for input from oracle
+    host.state_mut().state = SlotMachineEnum::ActiveGame;
+
+    host.state_mut().user_random_value = 50;
+
     Ok(())
 }
 
-/// Smash a piggy bank retrieving the CCD, only allowed by the owner.
-#[receive(contract = "PiggyBank", name = "smash", mutable)]
-fn piggy_smash<S: HasStateApi>(
-    ctx: &impl HasReceiveContext,
-    host: &mut impl HasHost<PiggyBankState, StateApiType = S>,
-) -> ReceiveResult<()> {
-    // Get the contract owner, i.e. the account who initialized the contract.
-    let owner = ctx.owner();
-    // Get the sender, who triggered this function, either a smart contract or
-    // an account.
-    let sender = ctx.sender();
-
-    // Ensure only the owner can smash the piggy bank.
-    ensure!(sender.matches_account(&owner));
-    // Ensure the piggy bank has not been smashed already.
-    ensure!(*host.state() == PiggyBankState::Intact);
-    // Set the state to be smashed.
-    *host.state_mut() = PiggyBankState::Smashed;
-
-    // Get the current balance of the smart contract.
-    let balance = host.self_balance();
-    // Result in a transfer of the whole balance to the contract owner.
-    Ok(host.invoke_transfer(&owner, balance)?)
-}
-
-/// View the state and balance of the piggy bank.
-#[receive(contract = "PiggyBank", name = "view")]
-fn piggy_view<S: HasStateApi>(
+/// View the state and balance of the slotmachine.
+#[receive(contract = "SlotMachine", name = "view")]
+fn slot_machine_view<S: HasStateApi>(
     _ctx: &impl HasReceiveContext,
-    host: &impl HasHost<PiggyBankState, StateApiType = S>,
-) -> ReceiveResult<(PiggyBankState, Amount)> {
+    host: &impl HasHost<SlotMachineState, StateApiType = S>,
+) -> ReceiveResult<(SlotMachineState, Amount)> {
     let current_state = *host.state();
     let current_balance = host.self_balance();
     Ok((current_state, current_balance))
